778fc8a40387ec75c3255cd8eeba02a4734cd843
Hana Zait <hana.o.zait@gmail.com>
2023-02-09 16:04:11 -0800

    Enable focus mode for MDN Docs and customize stylesheet depending on site. Rename Chrome stylsheet.

--- background.js
+++ background.js
@@ -6,9 +6,13 @@ chrome.runtime.onInstalled.addListener(() => {
 
 const extensions = 'https://developer.chrome.com/docs/extensions'
 const webstore = 'https://developer.chrome.com/docs/webstore'
+const mdnDocs = 'https://developer.mozilla.org/en-US/docs'
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+  // Check if URL starts with one of the specified URLs
+  if (tab.url.startsWith(extensions) ||
+    tab.url.startsWith(webstore) ||
+    tab.url.startsWith(mdnDocs)) {
     // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
     // Next state will always be the opposite
@@ -19,17 +23,27 @@ chrome.action.onClicked.addListener(async (tab) => {
       tabId: tab.id,
       text: nextState,
     });
-    
+
+    // Initialize stylesheet var
+    let stylesheet = undefined;
+
+    // Specify stylesheet depending on which site it is
+    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+      stylesheet = 'focus-mode-chrome.css';
+    } else if (tab.url.startsWith(mdnDocs)) {
+      stylesheet = 'focus-mode-mdn.css';
+    };
+
     if (nextState === "ON") {
       // Insert the CSS file when the user turns the extension on
       await chrome.scripting.insertCSS({
-        files: ["focus-mode.css"],
+        files: [stylesheet],
         target: { tabId: tab.id },
       });
     } else if (nextState === "OFF") {
       // Remove the CSS file when the user turns the extension off
       await chrome.scripting.removeCSS({
-        files: ["focus-mode.css"],
+        files: [stylesheet],
         target: { tabId: tab.id },
       });
     }
